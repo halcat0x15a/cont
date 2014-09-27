d0992e05af8f920c3c4bbb78d3a88e027f561817
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-09-28 02:33:13 +0900

    continuations

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject shift-reset "0.1.0-SNAPSHOT"
+  :description "Delimited continuations library"
+  :url "https://github.com/halcat0x15a/shift-reset"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
