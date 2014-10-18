0c9bbbecdfbb5eb34c9ca905a19ead1cfc0c4069
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-10-19 03:35:00 +0900

    optimization

--- src/cont/core.clj
+++ src/cont/core.clj
@@ -21,29 +21,34 @@
     (first exprs)))
 
 (defn transform [cont expr]
-  (if (seq? expr)
-    (apply application cont (macroexpand expr))
-    (cont expr)))
+    (cond (seq? expr) (apply application cont (macroexpand expr))
+          (symbol? expr) (let [e (gensym expr)] `(call ~expr (fn* [~e] ~(cont e))))
+          :else (cont expr)))
+
+(defn cps
+  ([cont] (cont ()))
+  ([cont expr & exprs]
+     (if exprs
+       (transform (fn [e] (apply cps (fn [es] (cont `(~e ~@es))) exprs)) expr)
+       (transform (fn [e] (cont `(~e))) expr))))
 
 (defmethod application 'if
   ([cont _ test then]
      (application cont 'if test then nil))
   ([cont _ test then else]
-     (transform (fn [expr] `(call ~expr (fn [param#] (if param# ~(transform cont then) ~(transform cont else))))) test)))
+     (transform (fn [e] `(if ~e ~(transform cont then) ~(transform cont else))) test)))
 
 (defmethod application 'do [cont _ & exprs]
   (if exprs
     (if (next exprs)
-      (transform (fn [expr] `(call ~expr (fn [_#] ~(apply application cont 'do (next exprs))))) (first exprs))
+      (transform (fn [_] (apply application cont 'do (next exprs))) (first exprs))
       (transform cont (first exprs)))
     (cont nil)))
 
 (defmethod application 'let* [cont _ bindings & exprs]
-  (let [bindings (partition 2 bindings)]
-    (reduce (fn [expr [param init]]
-              (transform (fn [init] `(call ~init (fn [~param] ~expr))) init))
-            (apply application cont 'do exprs)
-            (reverse bindings))))
+  (if (seq bindings)
+    (transform (fn [e] `(let* [~(first bindings) ~e] ~(apply application cont 'let* (nnext bindings) exprs))) (second bindings))
+    (apply application cont 'do exprs)))
 
 (defmethod application 'letfn* [cont _ & exprs]
   (apply application cont 'let* exprs))
@@ -52,33 +57,32 @@
   (apply application cont 'let* exprs))
 
 (defmethod application 'throw [cont _ expr]
-  (transform (fn [params] (cont `(throw ~params))) expr))
+  (transform (fn [e] (cont `(throw ~e))) expr))
 
 (defmethod application '. [cont _ expr method & exprs]
-  (if (and (symbol? expr) (class? (resolve expr)))
-    (apply application (fn [params] (cont `(. ~expr ~method ~@params))) exprs)
-    (let [param (gensym)]
-      (application (fn [param] (apply application (fn [params] (cont `(. ~@param ~method ~@exprs))) exprs)) expr))))
+  (let [a (gensym)]
+    (if (and (symbol? expr) (class? (resolve expr)))
+      (apply cps (fn [es] `(call (. ~expr ~method ~@es) (fn* [~a] ~(cont a)))) exprs)
+      (transform (fn [e] (apply cps (fn [es] `(call (. ~e ~method ~@es) (fn* [~a] ~(cont a)))) exprs)) expr))))
 
 (defmethod application 'new [cont _ class & exprs]
-  (apply application (fn [exprs] (cont `(new ~class ~@exprs))) exprs))
+  (let [a (gensym)]
+    (apply cps (fn [es] `(call (new ~class ~@es) (fn* [~a] ~(cont a)))) exprs)))
 
 (defmethod application 'set! [cont _ symbol expr]
-  (transform (fn [exprs] (cont `(set! ~symbol ~expr))) expr))
+  (transform (fn [e] (cont `(set! ~symbol ~e))) expr))
 
 (defmethod application nil [cont]
-  (cont '()))
+  (cont ()))
 
 (defmethod application :default [cont expr & exprs]
   (if (special-symbol? expr)
     (cont `(~expr ~@exprs))
-    (let [param (gensym)]
-      (if exprs
-        (transform (fn [expr] `(call ~expr (fn [~param] ~(apply application (fn [params] (cont `(~param ~@params))) exprs)))) expr)
-        (transform (fn [expr] `(call ~expr (fn [~param] ~(cont `(~param))))) expr)))))
+    (let [a (gensym)]
+      (apply cps (fn [e] `(call ~e (fn* [~a] ~(cont a)))) expr exprs))))
 
 (defmacro reset [& exprs]
-  (transform identity `(identity (do ~@exprs))))
+  (transform identity `(do ~@exprs)))
 
 (defmacro shift [param & exprs]
-  `(Context. (fn [~param] ~@exprs)))
+  `(Context. (fn* [~param] ~@exprs)))
