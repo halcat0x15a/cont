657d6273820c3dbfed85bfe69b709374c63e2dcd
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-10-23 15:06:14 +0900

    value class

--- src/main/scala/cont/package.scala
+++ src/main/scala/cont/package.scala
@@ -2,17 +2,17 @@ import scala.language.experimental.macros
 
 package object cont {
 
-  case class Context[+A, -B, +C](cc: (A => B) => C)
+  case class Context[+A, -B, +C](cc: (A => B) => C) extends AnyVal
 
   def shift[A, B, C](cc: (A => B) => C) = Context(cc)
 
-  implicit class CC[A](a: A) {
+  implicit class CC[A](val a: A) extends AnyVal {
     def cc[B](f: A => B) = f(a)
   }
 
   def reset(expr: Any): Any = macro resetImpl
 
-  def resetImpl(c: scala.reflect.macros.whitebox.Context)(expr: c.Expr[Any]): c.universe.Tree = {
+  def resetImpl(c: scala.reflect.macros.whitebox.Context)(expr: c.universe.Tree): c.universe.Tree = {
     import c.universe._
 
     def apply(expr: Tree, name: TermName)(cont: Tree => Tree) =
@@ -31,18 +31,16 @@ package object cont {
 	  es match {
 	    case Nil =>
 	      e match {
-		case q"$f[..$_](..$args)" =>
-		  application(args)(as => apply(q"$f(..$as)", name)(cont))
+		case q"$f[..$_](..$args)" => application(args)(as => apply(q"$f(..$as)", name)(cont))
 		case q"val $x: $t = $a" => transform(a, x)(cont)
 		case _ => apply(e, name)(cont)
 	      }
-	    case _ =>
-	      transform(e)(_ => transform(q"{ ..$es }")(cont))
+	    case _ => transform(e)(_ => transform(q"{ ..$es }")(cont))
 	  }
 	case _ => transform(q"{ $expr }")(cont)
       }
 
-    transform(c.untypecheck(expr.tree))(x => x)
+    transform(c.untypecheck(expr))(x => x)
   }
 
 }
