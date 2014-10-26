76195ec3bb1a0c3576a75921f23e05026fddd236
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-10-26 18:35:09 +0900

    move clojure directories

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/halcat0x15a/cont"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"])
